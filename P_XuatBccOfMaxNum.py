52a3a0ffbef677252ea848461c92823b0255443b
TamNhien <canhthien91@gmail.com>
2024-03-29 15:59:50 +0700

    Add project files.

---
+++ P_XuatBccOfMaxNum.py
@@ -0,0 +1,9 @@
+﻿def bang_cuu_chuong(a, b):
+    num = max(a, b)
+    for i in range(1, 10):
+        print(f"{num} x {i} = {num * i}")
+
+num1 = int(input("Nhập số nguyên thứ nhất: "))
+num2 = int(input("Nhập số nguyên thứ hai: "))
+bang_cuu_chuong(num1, num2)
+
